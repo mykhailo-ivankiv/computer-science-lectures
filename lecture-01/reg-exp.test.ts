f2c9df2ece6a113b88e4374d62c56f2a8dc6fc4f
mivankiv <mivankiv@degiro.nl>
2020-07-26 16:23:53 +0200

    Initial commit.

---
+++ lecture-01/reg-exp.test.ts
@@ -0,0 +1,30 @@
+import { assertEquals, assertArrayContains } from 'https://deno.land/std/testing/asserts.ts';
+import isValidCSS from './css-on-reg-exp.ts';
+
+// Simple name and function, compact form, but not configurable
+Deno.test(`is css`, () => {
+  assertEquals(isValidCSS(``), true);
+  assertEquals(isValidCSS(`\n   \n\n`), true);
+  assertEquals(isValidCSS(`div{}`), true);
+  assertEquals(isValidCSS(`div {}`), true);
+  assertEquals(isValidCSS(`div {}`), true);
+  assertEquals(
+    isValidCSS(`
+    div 
+    { 
+    }
+    `),
+    true
+  );
+});
+
+Deno.test(`isn't css`, () => {
+  assertEquals(isValidCSS(`{}`), false);
+  assertEquals(
+    isValidCSS(`
+    div {}
+    {}
+  `),
+    false
+  );
+});
